efb5660c40c3ac6a4a5643a8f49010cab0939169
Taner-Gokcekk <tanergokcek1@outlook.com>
2025-05-01 19:57:42 +0300

    1.day
    Giriş yapıldı.İlk 9 video izlendi.

---
+++ 1.Eklentilerin Kurulması/1.Eklentilerin Kurulması/app.js
@@ -0,0 +1,16 @@
+//Yorum satırı
+
+// * Enes
+// ?Enes
+// ! Burayı kontrol etmeyi unutmayın.
+// TODO : Buraya if koşulu eklemeyi unutma...
+
+
+// console.log("Enes");
+
+// console.log("Enes");
+// console.log("Enes");
+// console.log("Enes");
+// console.log("Enes");
+// console.log("Enes");
+// console.log("Enes");
