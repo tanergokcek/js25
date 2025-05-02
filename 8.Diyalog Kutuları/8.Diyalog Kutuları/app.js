b36a14e852d741d818bb7a7a1f43b978e7dc7941
Taner-Gokcekk <tanergokcek1@outlook.com>
2025-05-02 22:33:29 +0300

    2.Day
    Veri tipleri-Operatörler-İflere bakıldı.

---
+++ 8.Diyalog Kutuları/8.Diyalog Kutuları/app.js
@@ -0,0 +1,31 @@
+/*
+    --------DİYALOG KUTULARI---------
+     - Alert
+     - Prompt
+     - Confirm
+*/
+
+
+// console.log(window);
+
+
+// alert("İşleminize devam etmeden önce kaydetmelisiniz!");
+
+
+//Prompt(); Kullanıcıdan değer alabiliyoruz...
+
+//Kullanıcıdan alınan değerler her zaman string döner.
+// let isim = prompt("İsminizi Giriniz :");
+// let yas  = prompt("Yaşınızı giriniz : ");
+
+//TÜR DÖNÜŞÜMLERİ adındaki konu ile.
+
+// console.log("İsminiz : " + isim);
+// console.log("Yaşınız : "  + yas);
+
+// console.log(typeof isim);
+// console.log(typeof yas);
+
+
+// let sonuc =confirm("Silmek istediğinize emin misiniz ?");
+// console.log(sonuc);
